2451b2d9ad8f398d5651ec560d6a9eb852faef64
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-11 11:46:39 +0900

    Create fizzbuzz.rb

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,17 @@
+# ループ回数を設定
+Roop_amount = 20
+#1からカウント
+1.upto(Roop_amount).each do |count|
+  output = ""
+  if count%3==0
+    output += "Fizz"
+  end
+  if count%5==0
+    output += "Buzz"
+  end
+  # ここまでの処理でFizzかBuzzに該当しているか
+  if output.empty?
+    puts count
+  else puts output
+  end
+end
