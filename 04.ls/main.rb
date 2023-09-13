59b04c7e39e43d18c2030ef90d4c887072ddde88
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-13 12:13:01 +0900

    ソート実装

--- 04.ls/main.rb
+++ 04.ls/main.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
+output = []
 Dir.foreach('.') do |item|
   next if item == '.' or item == '..'
   if !item.start_with?("\.")
-    puts item
+    output.push(item)
   end
 end
+puts(output.sort)
