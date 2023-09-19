d69ade2d2537f2d72d2bcce854a36d9c099a3a10
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-19 17:47:58 +0900

    全体的に変数名を修正しました。
    コメントがなくても読めるよう、どういったものが格納されているのかがわかるようにしました。

--- 04.ls/main.rb
+++ 04.ls/main.rb
@@ -10,35 +10,36 @@ def current_directory_content_names
   end
 end
 
-def sort_like_ls(arr)
-  sorted_arr = []
-  max_lines = (arr.size / AMOUNT_PER_LINE.to_f).ceil
-  max_line_columb_count = (arr.size % AMOUNT_PER_LINE)
+def sort_like_ls(content_names)
+  sorted_content_names = []
+  max_number_of_lines = (content_names.size / AMOUNT_PER_LINE.to_f).ceil
+  amount_of_max_line_column = (content_names.size % AMOUNT_PER_LINE)
 
-  arr = arr.sort
-  max_line_columb_count.times do |line|
-    sorted_arr.push(arr[(line * max_lines), max_lines])
+  simple_sorted_content_names = content_names.sort
+  amount_of_max_line_column.times do |line|
+    sorted_content_names.push(simple_sorted_content_names[(line * max_number_of_lines), max_number_of_lines])
   end
 
-  part_of_arr = arr[max_line_columb_count * max_lines..]
+  content_without_max_line_column = simple_sorted_content_names[amount_of_max_line_column * max_number_of_lines..]
 
-  other_part_of_arr = if max_line_columb_count.zero?
-                        make_divided_arr(part_of_arr, max_lines, 0)
-                      else
-                        make_divided_arr(part_of_arr, max_lines - 1, max_line_columb_count)
-                      end
-  sorted_arr.concat(other_part_of_arr)
+  divided_content_without_max_line_column = if amount_of_max_line_column.zero?
+                                              make_divided_content_names(content_without_max_line_column, max_number_of_lines, 0)
+                                            else
+                                              make_divided_content_names(content_without_max_line_column, max_number_of_lines - 1, amount_of_max_line_column)
+                                            end
+  sorted_content_names.concat(divided_content_without_max_line_column)
 
-  sorted_arr.map! { |it| it.values_at(0...max_lines) }
-  sorted_arr.transpose
+  # 行・列の要素数が異なると転置できないため、nilで埋めています。
+  filled_nil_sorted_content_names = sorted_content_names.map { |it| it.values_at(0...max_number_of_lines) }
+  filled_nil_sorted_content_names.transpose
 end
 
-def make_divided_arr(part_of_arr, divid_count, max_line_columb_count)
-  divided_arr = []
-  (AMOUNT_PER_LINE - max_line_columb_count + 1).times do |line|
-    divided_arr.push(part_of_arr[(line * divid_count), divid_count])
+def make_divided_content_names(content_without_max_line_column, divid_count, amount_of_max_line_column)
+  divided_content_names = []
+  (AMOUNT_PER_LINE - amount_of_max_line_column + 1).times do |line|
+    divided_content_names.push(content_without_max_line_column[(line * divid_count), divid_count])
   end
-  divided_arr
+  divided_content_names
 end
 
 content_names = current_directory_content_names
@@ -46,6 +47,6 @@ max_content_name_length = content_names.map(&:length).max
 sorted_content_names = sort_like_ls(content_names)
 
 sorted_content_names.each do |sorted_content_name|
-  sorted_content_name.each { |v| print "%-#{max_content_name_length+1}s" % v }
+  sorted_content_name.each { |v| print "%-#{max_content_name_length + 1}s" % v }
   puts
 end
