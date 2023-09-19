55df7194769a56242e01f065024cd6d91b471243
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-19 10:29:20 +0900

    曜日を取得するメソッドを削除
    Date.wday処理を利用。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -40,32 +40,22 @@ def get_show_ym
 end
 
 #--------------------------------
-# 引数で指定された日付の曜日を返します。
-# 返値:0-6(日-土)
-#--------------------------------
-def get_wday(year,month,day)
-  date = Date.new(year,month,day)
-  date.wday
-end
-
-#--------------------------------
 # メイン処理
 #--------------------------------
 # 表示対象年月を取得
 show_ym = get_show_ym
 show_year = show_ym["year"]
 show_month =show_ym["month"] 
-# 表示対象年月の末日を取得
-last_date = Date.new(year,month,-1)
-# 1日の曜日を取得
-wday_of_the_first_day = get_wday(show_year,show_month,1)
+# 表示対象年月の初日・末日を取得
+first_date = Date.new(show_year,show_month,1)
+last_date = Date.new(show_year,show_month,-1)
 
 # 出力
 puts("#{show_month}月#{show_year}".center(20))
 ["日","月","火","水","木","金","土"].each{|day| printf("%2s".%(day))}
 puts()
 # 1日の表示開始位置(曜日)まで空白埋め
-wday_of_the_first_day.times.each{printf("%3s".%(""))}
+first_date.wday.times.each{printf("%3s".%(""))}
 (Date.new(show_year, show_month, 1)..Date.new(show_year, show_month, last_date.mday)).each do |date|
   print "%3d" % date.strftime("%e")
   if date.wday%7 == 6
