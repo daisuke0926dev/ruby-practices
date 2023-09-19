9fd34cf8b79cc347837f86c754afc8a735f5f645
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-19 18:31:33 +0900

    変数名を適切に(複数形にしたり)

--- 04.ls/main.rb
+++ 04.ls/main.rb
@@ -20,25 +20,25 @@ def vertical_sort(content_names)
     sorted_content_names.push(simple_sorted_content_names[(line * max_number_of_lines), max_number_of_lines])
   end
 
-  content_without_max_line_column = simple_sorted_content_names[amount_of_max_line_column * max_number_of_lines..]
+  content_names_without_max_line_column = simple_sorted_content_names[amount_of_max_line_column * max_number_of_lines..]
 
   # 転置後、縦に連番させるために、配列を区切ります
-  divided_content_without_max_line_column = if amount_of_max_line_column.zero?
-                                              make_divided_content_names(content_without_max_line_column, max_number_of_lines, 0, limit_per_line)
+  divided_content_names_without_max_line_column = if amount_of_max_line_column.zero?
+                                              make_divided_content_names(content_names_without_max_line_column, max_number_of_lines, 0, limit_per_line)
                                             else
-                                              make_divided_content_names(content_without_max_line_column, max_number_of_lines - 1, amount_of_max_line_column, limit_per_line)
+                                              make_divided_content_names(content_names_without_max_line_column, max_number_of_lines - 1, amount_of_max_line_column, limit_per_line)
                                             end
-  sorted_content_names.concat(divided_content_without_max_line_column)
+  sorted_content_names.concat(divided_content_names_without_max_line_column)
 
   # 行・列の要素数が異なると転置できないため、nilで埋めています。
   filled_nil_sorted_content_names = sorted_content_names.map { |it| it.values_at(0...max_number_of_lines) }
   filled_nil_sorted_content_names.transpose
 end
 
-def make_divided_content_names(content_without_max_line_column, divid_count, amount_of_max_line_column, limit_per_line)
+def make_divided_content_names(content_names_without_max_line_column, divid_count, amount_of_max_line_column, limit_per_line)
   divided_content_names = []
   (limit_per_line - amount_of_max_line_column + 1).times do |line|
-    divided_content_names.push(content_without_max_line_column[(line * divid_count), divid_count])
+    divided_content_names.push(content_names_without_max_line_column[(line * divid_count), divid_count])
   end
   divided_content_names
 end
