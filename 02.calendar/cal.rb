7d334608613a94f3208424616fc7cf52c56dae2e
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-19 10:37:45 +0900

    rubocopが通るように
    ・書式付き文字列の書き方を統一
    ・メソッド名変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'optparse'
 require 'date'
 
@@ -6,7 +8,7 @@ require 'date'
 # コマンドラインオプションで年月を受け取ります。(省略可)
 # 返値：ハッシュ["year":yyyy, "month":mm(0埋め無し)]
 # ----------------------------------------------
-def get_show_ym
+def receive_ym_from_cmd_line_option
   ret_date = { 'year' => Time.now.year.to_i, 'month' => Time.now.month.to_i }
   opt = OptionParser.new
   # 年の指定
@@ -38,7 +40,7 @@ end
 # メイン処理
 #--------------------------------
 # 表示対象年月を取得
-show_ym = get_show_ym
+show_ym = receive_ym_from_cmd_line_option
 show_year = show_ym['year']
 show_month = show_ym['month']
 # 表示対象年月の初日・末日を取得
@@ -47,12 +49,12 @@ last_date = Date.new(show_year, show_month, -1)
 
 # 出力
 puts("#{show_month}月#{show_year}".center(20))
-%w[日 月 火 水 木 金 土].each { |day| printf('%2s'.%(day)) }
+%w[日 月 火 水 木 金 土].each { |day| print(format('%2s', day)) }
 puts
 # 1日の表示開始位置(曜日)まで空白埋め
-first_date.wday.times.each { printf(format('%3s', '')) }
+first_date.wday.times.each { print(format('%3s', '')) }
 (Date.new(show_year, show_month, 1)..Date.new(show_year, show_month, last_date.mday)).each do |date|
-  print '%3d' % date.strftime('%e')
+  print(format('%3d', date.strftime('%e')))
   # 土曜日であれば改行
   puts if date.wday % 7 == 6
 end
