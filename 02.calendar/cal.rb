960066172f55df7e417953e4f0575bbb412e17aa
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-19 10:30:22 +0900

    rubocop -a

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,35 +1,30 @@
 #!/usr/bin/env ruby
 require 'optparse'
-require "date"
+require 'date'
 
 # ----------------------------------------------
 # コマンドラインオプションで年月を受け取ります。(省略可)
 # 返値：ハッシュ["year":yyyy, "month":mm(0埋め無し)]
 # ----------------------------------------------
 def get_show_ym
-
-  ret_date = {"year"=>Time.now.year.to_i ,"month"=>Time.now.month.to_i}
+  ret_date = { 'year' => Time.now.year.to_i, 'month' => Time.now.month.to_i }
   opt = OptionParser.new
   # 年の指定
   opt.on('-y', '--year [ITEM]', 'select year') do |param_year|
-    if param_year != nil
-      ret_date["year"] = param_year.to_i
-    end
+    ret_date['year'] = param_year.to_i if !param_year.nil?
     # パラメータチェック
-    if ret_date["year"] < 1970 || ret_date["year"] > 2100
-      puts("年の指定に誤りがあります。")
+    if ret_date['year'] < 1970 || ret_date['year'] > 2100
+      puts('年の指定に誤りがあります。')
       exit
     end
   end
-  
+
   # 月の指定
   opt.on('-m', '--month [ITEM]', 'select month') do |param_month|
-    if param_month != nil
-      ret_date["month"] = param_month.to_i
-    end
+    ret_date['month'] = param_month.to_i if !param_month.nil?
     # パラメータチェック
-    if ret_date["month"] < 1 || ret_date["month"] > 12
-      puts("月の指定に誤りがあります。")
+    if ret_date['month'] < 1 || ret_date['month'] > 12
+      puts('月の指定に誤りがあります。')
       exit
     end
   end
@@ -44,23 +39,21 @@ end
 #--------------------------------
 # 表示対象年月を取得
 show_ym = get_show_ym
-show_year = show_ym["year"]
-show_month =show_ym["month"] 
+show_year = show_ym['year']
+show_month = show_ym['month']
 # 表示対象年月の初日・末日を取得
-first_date = Date.new(show_year,show_month,1)
-last_date = Date.new(show_year,show_month,-1)
+first_date = Date.new(show_year, show_month, 1)
+last_date = Date.new(show_year, show_month, -1)
 
 # 出力
 puts("#{show_month}月#{show_year}".center(20))
-["日","月","火","水","木","金","土"].each{|day| printf("%2s".%(day))}
-puts()
+%w[日 月 火 水 木 金 土].each { |day| printf('%2s'.%(day)) }
+puts
 # 1日の表示開始位置(曜日)まで空白埋め
-first_date.wday.times.each{printf("%3s".%(""))}
+first_date.wday.times.each { printf(format('%3s', '')) }
 (Date.new(show_year, show_month, 1)..Date.new(show_year, show_month, last_date.mday)).each do |date|
-  print "%3d" % date.strftime("%e")
-  if date.wday%7 == 6
-    # 土曜日であれば改行
-    puts()
-  end
+  print '%3d' % date.strftime('%e')
+  # 土曜日であれば改行
+  puts if date.wday % 7 == 6
 end
-puts()
+puts
