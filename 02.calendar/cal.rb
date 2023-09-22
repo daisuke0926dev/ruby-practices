02bc49e8c4e255dcda29d31bfa7dcf271baa5c15
dai <dev_daisuke0926@yahoo.co.jp>
2023-09-22 18:20:35 +0900

    ハッシュの書き方が誤っており、コマンドラインオプションを受け付けなくなっていたので、解消

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -13,9 +13,9 @@ def receive_ym_from_cmd_line_option
   opt = OptionParser.new
   # 年の指定
   opt.on('-y', '--year [ITEM]', 'select year') do |param_year|
-    ret_date['year'] = param_year.to_i if !param_year.nil?
+    ret_date[:year] = param_year.to_i if !param_year.nil?
     # パラメータチェック
-    if ret_date['year'] < 1970 || ret_date['year'] > 2100
+    if ret_date[:year] < 1970 || ret_date[:year] > 2100
       puts('年の指定に誤りがあります。')
       exit
     end
@@ -23,9 +23,9 @@ def receive_ym_from_cmd_line_option
 
   # 月の指定
   opt.on('-m', '--month [ITEM]', 'select month') do |param_month|
-    ret_date['month'] = param_month.to_i if !param_month.nil?
+    ret_date[:month] = param_month.to_i if !param_month.nil?
     # パラメータチェック
-    if ret_date['month'] < 1 || ret_date['month'] > 12
+    if ret_date[:month] < 1 || ret_date[:month] > 12
       puts('月の指定に誤りがあります。')
       exit
     end
@@ -53,7 +53,7 @@ puts("#{show_month}月#{show_year}".center(20))
 puts
 # 1日の表示開始位置(曜日)まで空白埋め
 first_date.wday.times.each { print(format('%3s', '')) }
-(first_date)..last_date).each do |date|
+(first_date..last_date).each do |date|
   print(format('%3d', date.strftime('%e')))
   # 土曜日であれば改行
   puts if date.wday == 6
