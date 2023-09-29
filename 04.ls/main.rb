a19a16da3ca88447715b3c341630304a745f10ba
dai <90462400+daisuke0926dev@users.noreply.github.com>
2023-09-29 11:21:09 +0900

    Revert "04ls -rの実装"

--- 04.ls/main.rb
+++ 04.ls/main.rb
@@ -2,13 +2,12 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require 'etc'
 
 MAX_COLUMN = 3
 
 # ファイル名ディレクトリ名を包括するので、コンテンツと称しています。
-def current_directory_content_names(options)
-  options[:option_show_hidden_files] ? Dir.foreach('.').to_a : Dir.foreach('.').reject { |content_name| content_name.start_with?('.') }
+def current_directory_content_names(option_hash)
+  option_hash[:option_lower_a] ? Dir.foreach('.').to_a : Dir.foreach('.').reject { |content_name| content_name.start_with?('.') }
 end
 
 def sort_vertically(content_names)
@@ -17,11 +16,12 @@ def sort_vertically(content_names)
   limit_per_line = (content_names.size / max_number_of_lines.to_f).ceil
   amount_of_max_line_column = limit_per_line - ((max_number_of_lines * limit_per_line) % content_names.size)
 
+  simple_sorted_content_names = content_names.sort
   amount_of_max_line_column.times do |line|
-    sorted_content_names.push(content_names[(line * max_number_of_lines), max_number_of_lines])
+    sorted_content_names.push(simple_sorted_content_names[(line * max_number_of_lines), max_number_of_lines])
   end
 
-  content_names_without_max_line_column = content_names[amount_of_max_line_column * max_number_of_lines..]
+  content_names_without_max_line_column = simple_sorted_content_names[amount_of_max_line_column * max_number_of_lines..]
 
   # 転置後、縦に連番させるために、配列を区切ります
   divided_content_names_without_max_line_column =
@@ -46,95 +46,21 @@ def make_divided_content_names(content_names_without_max_line_column, divid_coun
 end
 
 def parse_command_line_option
-  option_show_hidden_files = false
-  option_reverse = false
-  option_detailed_listing = false
+  option_lower_a = false
 
   opt = OptionParser.new
-  opt.on('-a', '--all', 'show all items') { option_show_hidden_files = true }
-  opt.on('-r', '--reverse', 'show reverse items') { option_reverse = true }
-  opt.on('-l', '', 'detailed list of items') { option_detailed_listing = true }
+  opt.on('-a', '--add', 'add an item') { option_lower_a = true }
   opt.parse(ARGV)
-  { option_show_hidden_files:, option_reverse:, option_detailed_listing: }
-end
-
-def sort_with_details(content_names)
-  total_block_size = 0
-  detailed_contents = Array.new(content_names.size)
-
-  content_names.each_with_index do |content_name, index|
-    file_stat = get_file_stat(content_name)
-    detailed_contents[index] = build_detailed_content(file_stat, content_name)
-    total_block_size += (file_stat.size / 512).ceil
-  end
-
-  [detailed_contents, total_block_size]
-end
-
-def get_file_stat(content_name)
-  File.stat(File.join(Dir.pwd, content_name))
-end
-
-def build_detailed_content(file_stat, content_name)
-  [
-    file_type_and_permissions(file_stat),
-    file_stat.nlink.to_s,
-    Etc.getpwuid(file_stat.uid).name,
-    Etc.getgrgid(file_stat.gid).name,
-    file_stat.size.to_s.rjust(5),
-    file_stat.mtime.strftime('%-m').rjust(2),
-    file_stat.mtime.strftime('%-d').rjust(2),
-    file_stat.mtime.strftime('%R'),
-    content_name
-  ]
-end
-
-def file_type_and_permissions(file_stat)
-  type = file_stat.ftype[0] == 'd' ? 'd' : '-'
-  permissions = format('%6d', file_stat.mode.to_s(8))[3, 3].chars.map { |num| translate_permission_number_to_text(num.to_i) }.join
-  type + permissions
-end
-
-def translate_permission_number_to_text(permission_number)
-  binary_representation = format('%03d', permission_number.to_s(2))
-  binary_representation.chars.map.with_index { |bit, index| bit.to_i.zero? ? '-' : 'rwx'[index] }.join
-end
-
-def display_sorted_contents(sorted_content_names_with_details, block_size)
-  max_lengths = calculate_max_lengths(sorted_content_names_with_details)
-
-  puts("total #{block_size}")
-  sorted_content_names_with_details.each do |sub_array|
-    sub_array.each_with_index do |item, index|
-      print format("%-#{max_lengths[index] + 1}s", item)
-    end
-    puts
-  end
-end
-
-def calculate_max_lengths(sorted_content_names_with_details)
-  max_lengths = Array.new(sorted_content_names_with_details[0].size, 0)
-
-  sorted_content_names_with_details.each do |sub_array|
-    sub_array.each_with_index do |item, index|
-      max_lengths[index] = [max_lengths[index], item.length].max
-    end
-  end
-
-  max_lengths
+  { option_lower_a: }
 end
 
 options = parse_command_line_option
+
 content_names = current_directory_content_names(options)
-simple_sorted_content_names = options[:option_reverse] ? content_names.sort.reverse : content_names.sort
-if options[:option_detailed_listing]
-  sorted_content_names_with_details, block_size = sort_with_details(simple_sorted_content_names)
-  display_sorted_contents(sorted_content_names_with_details, block_size)
-else
-  sorted_content_names = sort_vertically(simple_sorted_content_names)
-  max_content_name_length = content_names.map(&:length).max
-  sorted_content_names.each do |sorted_content_name|
-    sorted_content_name.each { |v| print format("%-#{max_content_name_length + 1}s", v) }
-    puts
-  end
+max_content_name_length = content_names.map(&:length).max
+sorted_content_names = sort_vertically(content_names)
+
+sorted_content_names.each do |sorted_content_name|
+  sorted_content_name.each { |v| print format("%-#{max_content_name_length + 1}s", v) }
+  puts
 end
